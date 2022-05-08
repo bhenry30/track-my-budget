883275f1022ba2e1034269484918890df885036f
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-05-08 14:38:52 -0500

    adds idb javascript file that adds IndexedDB functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,60 @@
+const { response } = require("express");
+const { post } = require("../../routes/api");
+
 let db;
 
 const request = indexedDB.open('track_my_budget', 1)
+
+request.onupgradeneeded = e => {
+    const db = e.target.result;
+    db.createObjectStore('pending', { autoIncrement: True })
+}
+
+request.onsuccess = e => {
+ db = e.target.result
+ if (navigator.onLine) {
+     // upload
+ }
+}
+
+request.onerror = e => {
+    console.log(e.target.errorCode)
+}
+
+const saveRecord = record => {
+    const transaction = db.transaction(['pending'], 'readwrite')
+    const store = transaction.objectStore('pending')
+    store.add(record)
+}
+
+const checkDatabase = () => {
+    const transaction = db.transaction(['pending'], 'readwrite')
+    const store = transaction.objectStore('pending')
+    const getAll = store.getAll()
+
+    getAll.onsucess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: 'POST',
+                body: JSON.stringify(getAll.results),
+                headers : {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+                const transaction = db.transaction(['pending'], 'readwrite');
+                const store = transaction.objectStore('pending');
+                store.clear();
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+window.addEventListener('online', checkDatabase)
